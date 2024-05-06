bf71c68db4833beb2bd56b6463db724bdcbae020
Misha <nalbandyanmisha@gmail.com>
2024-05-06 19:04:42 +0400

    Add neorg

--- lua/community.lua
+++ lua/community.lua
@@ -7,5 +7,6 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  { import = "astrocommunity.note-taking.neorg" },
   -- import/override with your plugins folder
 }
