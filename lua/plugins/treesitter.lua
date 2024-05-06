a21f64a34be39753b0c8cd4b48be17f7ecde0b2f
Misha <nalbandyanmisha@gmail.com>
2024-05-06 18:07:56 +0400

    Add nvim-treesiter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,7 +8,46 @@ return {
     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
       "lua",
       "vim",
+      "rust",
+      "solidity",
+      "javascript",
       -- add more arguments for adding more treesitter parsers
     })
+
+    opts.auto_install = true
+    opts.highlight = {
+      enable = true,
+    }
+    opts.incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<Leader>ss",
+        node_incremental = "<Leader>si",
+        scope_incremental = "<Leader>sc",
+        node_decremental = "<Leader>sd",
+      },
+    }
+    opts.textobjects = {
+      enable = true,
+      select = {
+        enable = true,
+
+        lookahead = true,
+
+        keymaps = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@class.outer",
+          ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+          ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+        },
+        selection_modes = {
+          ["@parameter.outer"] = "v", -- charwise
+          ["@function.outer"] = "V", -- linewise
+          ["@class.outer"] = "<c-v>", -- blockwise
+        },
+        include_surrounding_whitespace = true,
+      },
+    }
   end,
 }
